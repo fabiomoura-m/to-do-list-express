227398e81381ebbd67588b582af4cddccc60113c
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-12 15:51:38 -0300

    Dados através das rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -5,10 +5,10 @@ const Checklist = require('../models/checklist');
 
 router.get('/', async (req, res) => {
     try {
-        const checklist = await Checklist.find({});
-        res.status(200).json(checklist);
+        const checklists = await Checklist.find({});
+        res.status(200).render('checklists/index', {checklists: checklists});
     } catch (error) {
-        res.status(500).json(error);
+        res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'});
     }
 });
 
@@ -28,9 +28,9 @@ router.get('/:id', async (req, res) => {
 
     try {
         const checklist = await Checklist.findById(id);
-        res.status(200).json(checklist);
+        res.status(200).render('checklists/show', {checklist: checklist});
     } catch (error) {
-        res.status(422).json(error);
+        res.status(500).render('pages/error', {error: 'Erro ao exibir as Listas de tarefas'});
     }
 });
 

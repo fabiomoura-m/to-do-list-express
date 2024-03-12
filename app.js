a57d89b33769aaf5f6dce906dac8921e2893cc0b
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-12 11:25:44 -0300

    Configuração EJS com express

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path')
+
+const rootRouter = require('./src/routes/index')
 const checkListRouter = require('./src/routes/checklist')
 require('./config/database')
 
 const app = express();
 app.use(express.json());
 
-app.use('/checklists', checkListRouter)
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
 
+app.use('/', rootRouter)
+app.use('/checklists', checkListRouter)
 
 
 app.listen(3000, () => {
